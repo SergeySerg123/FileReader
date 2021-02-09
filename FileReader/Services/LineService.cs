45c767b31429700857799b85e9efc3ff78469e42
Sergey <sergey_chizhik@ukr.net>
2021-02-09 12:08:22 +0200

    Create LineService.cs

---
+++ FileReader/Services/LineService.cs
@@ -0,0 +1,35 @@
+﻿using FileReader.Models;
+using FileReader.Repositories;
+
+namespace FileReader.Services
+{
+    public class LineService
+    {
+        private readonly LineRepository _repository;
+
+        public LineService(LineRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void AddWordsToLine(int lineNumber, string[] words)
+        {
+            if (words.Length > 0)
+            {
+                var line = _repository.Get(lineNumber);
+
+                if (line == null)
+                {
+                    line = Line.Create(lineNumber);
+                    _repository.Add(line);
+                }
+
+                foreach (var w in words)
+                {
+                    var word = Word.Create(lineNumber, w);
+                    line.Words.Add(word);
+                }
+            }
+        }
+    }
+}
