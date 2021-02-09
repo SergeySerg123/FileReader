309bce778f0d4f0d9984c9d1bbfc93c78655c9c7
Sergey <sergey_chizhik@ukr.net>
2021-02-09 13:05:19 +0200

    Create ComposeService.cs

---
+++ FileReader/Services/ComposeService.cs
@@ -0,0 +1,35 @@
+﻿using FileReader.Repositories;
+
+namespace FileReader.Services
+{
+    public class ComposeService
+    {
+        private readonly LineRepository _lineRepository;
+        private readonly WordRepository _wordRepository;
+
+        public ComposeService(LineRepository lineRepository,
+            WordRepository wordRepository)
+        {
+            _lineRepository = lineRepository;
+            _wordRepository = wordRepository;
+        }
+
+        public void Compose()
+        {
+            foreach(var line in _lineRepository.Lines)
+            {
+                line.Words.ForEach(word =>
+                {
+                    var IsContains = _wordRepository.IsContainsValue(word);
+                    if (IsContains)
+                    {
+                        _wordRepository.UpdateWord(word, word.Line);
+                    } else
+                    {
+                        _wordRepository.AddWord(word);
+                    }
+                });
+            }
+        }
+    }
+}
