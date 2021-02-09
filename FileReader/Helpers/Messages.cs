753f3c809e2e758039d8ce8c93526918e1949f68
Sergey <sergey_chizhik@ukr.net>
2021-02-09 12:08:25 +0200

    Create Messages.cs

---
+++ FileReader/Helpers/Messages.cs
@@ -0,0 +1,7 @@
+﻿namespace FileReader.Helpers
+{
+    public static class Messages
+    {
+        public const string HelloMessage = "Hello! Please, move your file to command window! =)";
+    }
+}
