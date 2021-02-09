2ab13ed8d5ade86d0ca35e6add340784e5fe66b4
Sergey <sergey_chizhik@ukr.net>
2021-02-09 14:51:44 +0200

    Create Settings.cs

---
+++ FileReader/Helpers/Settings.cs
@@ -0,0 +1,9 @@
+﻿using System.IO;
+
+namespace FileReader.Helpers
+{
+    public static class Settings
+    {
+        public static readonly string _outputPath = $@"{Directory.GetCurrentDirectory()}..\..\..\..\Words.log";
+    }
+}
