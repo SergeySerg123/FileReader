117d560a2d158924669a58c838cba89ffb4e34fd
Sergey <sergey_chizhik@ukr.net>
2021-02-09 12:08:35 +0200

    Create Word.cs

---
+++ FileReader/Models/Word.cs
@@ -0,0 +1,17 @@
+﻿namespace FileReader.Models
+{
+    public class Word
+    {
+        public int Line { get; private set; }
+        public string Value { get; private set; }
+       
+        private Word(int lineNumber, string word) 
+        {
+            Line = lineNumber;
+            Value = word;  
+        }
+
+        // Factory method
+        public static Word Create(int lineNumber, string word) => new Word(lineNumber, word);
+    }
+}
