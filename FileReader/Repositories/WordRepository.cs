d0ea5f1ee47f9f156ecd69aaf5d3c9ab30366408
Sergey <sergey_chizhik@ukr.net>
2021-02-09 13:05:23 +0200

    Create WordRepository.cs

---
+++ FileReader/Repositories/WordRepository.cs
@@ -0,0 +1,31 @@
+﻿using FileReader.Models;
+using System.Collections.Generic;
+
+namespace FileReader.Repositories
+{
+    public class WordRepository
+    {
+        public Dictionary<string, List<int>> Words { get; private set; } = new Dictionary<string, List<int>>();
+
+        public void AddWord(Word word)
+        {
+            var linesList = new List<int>
+            {
+                word.Line
+            };
+            Words.Add(word.Value, linesList);
+        }
+
+        public void UpdateWord(Word word, int newLine)
+        {
+            var lines = Words[word.Value];
+            lines.Add(newLine);
+            Words[word.Value] = lines;
+        }
+
+        public bool IsContainsValue(Word word)
+        {
+            return Words.ContainsKey(word.Value);
+        }
+    }
+}
