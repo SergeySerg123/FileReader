4c16fbffe767743fbc4140c8e79724536386c98f
Sergey <sergey_chizhik@ukr.net>
2021-02-09 12:08:30 +0200

    Create Line.cs

---
+++ FileReader/Models/Line.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace FileReader.Models
+{
+    public class Line
+    {
+        public int LineNumber { get; private set; }
+        public List<Word> Words { get; private set; }
+
+        private Line(int lineNumber)
+        {
+            LineNumber = lineNumber;
+            Words = new List<Word>();
+        }
+
+        // Factory method
+        public static Line Create(int lineNumber) => new Line(lineNumber);
+    }
+}
